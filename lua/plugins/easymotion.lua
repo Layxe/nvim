63531cf5bd37073da5c1df1fb0369eb81a16e110
Alexander Niedermayer <layxe.alex@gmail.com>
2024-11-20 15:56:38 +0100

    Adding easymotion

---
+++ lua/plugins/easymotion.lua
@@ -0,0 +1,21 @@
+return {
+    "smoka7/hop.nvim",
+    version = "*",
+    opts = {
+        keys = "etovxqdygfblzhckisuran"
+    },
+    init = function ()
+        local hop = require('hop')
+        local directions = require('hop.hint').HintDirection
+        hop.setup({
+            quit_key = "<SPC>"
+        })
+
+        vim.keymap.set("n", "<leader><leader>f", ":HopChar1AC<CR>")
+        vim.keymap.set("n", "<leader><leader>F", ":HopChar1BC<CR>")
+        vim.keymap.set("n", "<leader><leader>W", ":HopWordAC<CR>")
+        vim.keymap.set("n", "<leader><leader>B", ":HopWordBC<CR>")
+        vim.keymap.set("n", "<leader><leader>g", ":HopLine<CR>")
+        vim.keymap.set("n", "<leader><leader>/", ":HopPattern<CR>")
+    end
+}
