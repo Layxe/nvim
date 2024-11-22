3e5290a49f92020b537faf0cc70841b867ee4806
Alexander Niedermayer <layxe.alex@gmail.com>
2024-11-22 11:51:21 +0100

    Adding gf, to open in editor buffer rather than terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,3 +1,5 @@
+local is_terminal_open = false
+
 return {
     {
         'akinsho/toggleterm.nvim',
@@ -17,7 +19,38 @@ return {
             vim.keymap.set('t', '<C-u>',     [[<C-\><C-n><C-u>]]                  )
             vim.keymap.set('t', '<C-d>',     [[<C-\><C-n><C-d>]]                  )
 
-            require('toggleterm').setup({ })
+            require('toggleterm').setup({
+                on_open = function ()
+                    is_terminal_open = true
+                end,
+                on_close = function ()
+                    is_terminal_open = false
+                end,
+            })
+
+            vim.keymap.set('n', 'gf', function ()
+                -- Yank word under cursor into register f
+                vim.api.nvim_input("\"fyiW")
+
+                local complete_word  = vim.fn.getreg('f')
+
+                local path, line = string.match(complete_word, "(.-):(%d+)")
+
+                if path == nil then
+                    path = complete_word
+                end
+
+                if is_terminal_open then
+                    -- Close terminal buffer
+                    vim.api.nvim_input("<A-w>")
+                end
+
+                vim.api.nvim_input(":edit " .. path .. "<CR>")
+
+                if line ~= nil then
+                    vim.api.nvim_input(line .. "G")
+                end
+            end)
         end
     }
 }
