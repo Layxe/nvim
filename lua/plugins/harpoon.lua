7ce3e333ae9076a700551bf1aeac2e24f92add21
Layxe <layxe.alex@gmail.com>
2025-08-20 23:57:55 +0200

    fix: improving harpoon plugin

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,7 +13,10 @@ return {
             vim.api.nvim_input("mh")
             harpoon:list():add()
         end)
-        vim.keymap.set("n", "<leader>ml", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>ml", function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end
+        )
 
         local conf = require("telescope.config").values
         local function toggle_telescope(harpoon_files)
@@ -34,6 +37,36 @@ return {
 
         vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
             { desc = "Open harpoon window" })
+
+        -- Jump to marker h, when pressing <leader>#
+        vim.keymap.set("n", "<leader>h", function()
+            -- check if mark 'h' exists in current buffer
+            local pos = vim.fn.getpos("'h")
+            local lnum, col = pos[2], pos[3]
+
+            if lnum > 0 and col > 0 then
+                -- jump to 'h'
+                vim.cmd("normal! 'h")
+            else
+                toggle_telescope(harpoon:list())
+
+                -- This is a workaround, since the second buffer is the buffer,
+                -- we are jumping to
+                vim.api.nvim_create_autocmd("BufEnter", {
+                    once = true,
+                    callback = function()
+                        vim.api.nvim_create_autocmd("BufEnter", {
+                            once = true,
+                            callback = function()
+                                if vim.fn.line("'h") > 0 then
+                                    vim.cmd("normal! 'h")
+                                end
+                            end,
+                        })
+                    end,
+                })
+            end
+        end, { desc = "Jump to mark h or Harpoon menu" })
 
         -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
         -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
