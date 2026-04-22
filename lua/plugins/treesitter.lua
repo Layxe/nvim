da9709caf8fcd7e7a0b05d16c86706243afd563f
Layxe <layxe.alex@gmail.com>
2026-04-22 22:18:51 +0200

    fix: using new config for tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,92 +1,66 @@
 return {
-  "nvim-treesitter",
-  beforeAll = function()
-    _G.Paq.add({
-      {
-        "nvim-treesitter/nvim-treesitter",
-        branch = "main",
-        build = ":TSUpdate",
-      },
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        branch = "main",
-      },
-    })
-  end,
-  load = function(name)
-    vim.cmd.packadd(name)
-    vim.cmd.packadd("nvim-treesitter-textobjects")
-  end,
-  after = function()
-    -- Note that some queries have dependencies, but if a dependency is
-    -- deleted, it won't automatically be reinstalled
-    require("nvim-treesitter").install({
-      -- Bundled parsers
-      "c",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "query",
-      "vim",
-      "vimdoc",
-      "bash",
-      "comment",
-      "diff",
-      "python",
-      "todotxt",
-      "vhdl",
-      "xml",
-    })
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local ts = require 'nvim-treesitter'
+        local parsers = {
+            "bash",
+            "comment",
+            "c",
+            "cpp",
+            "css",
+            "diff",
+            "dockerfile",
+            "elixir",
+            "git_config",
+            "gitcommit",
+            "gitignore",
+            "go",
+            "html",
+            "http",
+            "javascript",
+            "jsdoc",
+            "json",
+            "json5",
+            "lua",
+            "make",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "regex",
+            "rst",
+            "rust",
+            "scss",
+            "ssh_config",
+            "sql",
+            "terraform",
+            "typst",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "yaml",
+        }
 
-    require("nvim-treesitter-textobjects").setup({
-      select = {
-        lookahead = true,
-      },
-    })
-
-    local function map(lhs, obj)
-      vim.keymap.set({ "x", "o" }, lhs, function()
-        require("nvim-treesitter-textobjects.select").select_textobject(
-          obj,
-          "textobjects"
-        )
-      end)
-    end
-
-    map("af", "@function.outer")
-    map("if", "@function.inner")
-    map("ac", "@class.inner")
-    map("ic", "@class.inner")
-    map("ar", "@parameter.inner")
-    map("ir", "@parameter.inner")
-    map("ak", "@block.inner")
-    map("ik", "@block.inner")
-
-    -- Register the todotxt parser to be used for text filetypes
-    vim.treesitter.language.register("todotxt", "text")
-
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function(ev)
-        if pcall(vim.treesitter.start) then
-          -- Set indentexpr for queries that have an indents.scm, check in
-          -- ~/.local/share/nvim/site/queries/QUERY/
-          -- Hopefully this will happen automatically in the future
-          vim.cmd("TSBufEnable highlight")
-          vim.cmd("TSBufEnable indent")
-          if
-            ({
-              c = true,
-              lua = true,
-              markdown = true,
-              python = true,
-              query = true,
-              xml = true,
-            })[ev.match]
-          then
-            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-          end
+        for _, parser in ipairs(parsers) do
+            ts.install(parser)
         end
-      end,
-    })
-  end,
+
+        local patterns = {}
+        for _, parser in ipairs(parsers) do
+            local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+            for _, pp in pairs(parser_patterns) do
+                table.insert(patterns, pp)
+            end
+        end
+
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = patterns,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
