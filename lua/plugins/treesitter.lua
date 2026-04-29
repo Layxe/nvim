6878797a956ad94b3fd0d4b11dfe57dfdcaf8eed
Layxe <layxe.alex@gmail.com>
2026-04-29 08:27:05 +0000

    feat: another tree-sitter change...

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,54 +3,25 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        local ts = require 'nvim-treesitter'
         local parsers = {
-            "bash",
-            "comment",
-            "c",
-            "cpp",
-            "css",
-            "diff",
-            "dockerfile",
-            "elixir",
-            "git_config",
-            "gitcommit",
-            "gitignore",
-            "go",
-            "html",
-            "http",
-            "javascript",
-            "jsdoc",
-            "json",
-            "json5",
-            "lua",
-            "make",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "regex",
-            "rst",
-            "rust",
-            "scss",
-            "ssh_config",
-            "sql",
-            "terraform",
-            "typst",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "yaml",
+            "bash","comment","c","cpp","css","diff","dockerfile","elixir",
+            "git_config","gitcommit","gitignore","go","html","http",
+            "javascript","jsdoc","json","json5","lua","make","markdown",
+            "markdown_inline","python","regex","rst","rust","scss",
+            "ssh_config","sql","terraform","typst","toml","tsx",
+            "typescript","vim","vimdoc","yaml",
         }
 
-        for _, parser in ipairs(parsers) do
-            ts.install(parser)
-        end
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = parsers,
+            highlight = {
+                enable = true,
+            },
+        }
 
         local patterns = {}
         for _, parser in ipairs(parsers) do
-            local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+            local parser_patterns = vim.treesitter.language.get_filetypes(parser) or {}
             for _, pp in pairs(parser_patterns) do
                 table.insert(patterns, pp)
             end
